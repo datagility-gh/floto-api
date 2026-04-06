a22c9b57d41d567a87b4b8371820ecd3a83513bf
James Fox <james.fox@datagility.co.uk>
2026-04-06 09:25:11 +0000

    Adding GetAsync method to NoteRepository

--- Floto.Api/Notes/NoteRepository.cs
+++ Floto.Api/Notes/NoteRepository.cs
@@ -6,26 +6,50 @@ namespace Floto.Api.Notes
 {
     public class NoteRepository : INoteRepository
     {
-        private readonly CosmosClient cosmosClient;
         private readonly CosmosDbSettings cosmosDbSettings;
+
+        private readonly Container container;
 
         public NoteRepository(
             CosmosClient cosmosClient,
             IOptions<CosmosDbSettings> options)
         {
             cosmosDbSettings = options.Value;
-            this.cosmosClient = cosmosClient;
+
+            var database = cosmosClient.GetDatabase(cosmosDbSettings.DatabaseId);
+
+            container = database.GetContainer(cosmosDbSettings.ContainerId);
         }
 
         public async Task<Note> CreateAsync(Note note)
         {
-            var database = cosmosClient.GetDatabase(cosmosDbSettings.DatabaseId);
-
-            var container = database.GetContainer(cosmosDbSettings.ContainerId);
-
             var response = await container.CreateItemAsync(note);
 
             return response.Resource;
+        }
+
+        public async Task<IEnumerable<Note>> GetAsync(DateOnly date)
+        {
+            var query = $"SELECT * FROM n WHERE n.date = @date";
+
+            var queryDefinition = new QueryDefinition(query)
+                .WithParameter("@date", date.ToString(Note.DateFormat));
+
+            using FeedIterator<Note> feed = container.GetItemQueryIterator<Note>(
+                queryDefinition
+            );
+
+            var items = new List<Note>();
+            while (feed.HasMoreResults)
+            {
+                var response = await feed.ReadNextAsync();
+                foreach (Note item in response)
+                {
+                    items.Add(item);
+                }
+            }
+
+            return items;
         }
     }
 }
--- Floto.Test/Integration/NoteRepositoryTest.cs
+++ Floto.Test/Integration/NoteRepositoryTest.cs
@@ -7,7 +7,12 @@ namespace Floto.Test.Integration;
 
 public class NoteRepositoryTest
 {
+    const string DatabaseId = "local-sqldb-floto";
+    const string ContainerId = "sql-cont-floto";
+
     private readonly INoteRepository noteRepository;
+
+    private readonly Container container;
 
     public NoteRepositoryTest()
     {
@@ -20,14 +25,18 @@ public class NoteRepositoryTest
             }
         };
 
-        CosmosClient cosmosClient = new CosmosClient(
+        var cosmosClient = new CosmosClient(
             "AccountEndpoint=http://localhost:8081;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGg==;",
             cosmosClientOptions);
 
+        container = cosmosClient
+                .GetDatabase(DatabaseId)
+                .GetContainer(ContainerId);
+
         CosmosDbSettings cosmosDbSettings = new CosmosDbSettings
         {
-            DatabaseId = "local-sqldb-floto",
-            ContainerId = "sql-cont-floto"
+            DatabaseId = DatabaseId,
+            ContainerId = ContainerId
         };
 
         noteRepository = new NoteRepository(cosmosClient, Options.Create(cosmosDbSettings));
@@ -36,20 +45,83 @@ public class NoteRepositoryTest
     [Fact(DisplayName = "CreateAsync creates a note successfully")]
     public async Task CreateAsync_CreatesNoteSuccessfully()
     {
-        // Arrange
-        Note note = new Note
+        Note note = null!;
+
+        try
         {
-            Category = NoteCategory.General,
-            Content = "This is a test note."
+            // Arrange
+            note = new Note
+            {
+                Category = NoteCategory.General,
+                Content = "This is a test note."
+            };
+
+            // Act
+            var createdNote = await noteRepository.CreateAsync(note);
+
+            // Assert
+            createdNote.Should().NotBeNull();
+            createdNote.Id.Should().Be(note.Id);
+            createdNote.Category.Should().Be(note.Category);
+            createdNote.Content.Should().Be(note.Content);
+        }
+        finally
+        {
+            // Clean up - delete the created note
+            await container.DeleteItemAsync<Note>(
+                note.Id,
+                new PartitionKey(note.Date));
+        }
+    }
+
+    [Fact(DisplayName = "GetAsync by date retrieves notes successfully")]
+    public async Task GetAsync_ByDate_RetrievesNotesSuccessfully()
+    {
+        IEnumerable<Note> notes = null!;
+        try
+        {
+            // Arrange
+            notes = GetTestNotes();
+            foreach (var note in notes)
+            {
+                await container.CreateItemAsync(note);
+            }
+
+            // Act
+            var retrievedNotes = await noteRepository.GetAsync(DateOnly.FromDateTime(DateTime.UtcNow));
+
+            // Assert
+            retrievedNotes.Should().NotBeNull();
+            retrievedNotes.Should().NotBeEmpty();
+            retrievedNotes.Should().HaveCount(notes.Count());
+            retrievedNotes.First().Should().BeEquivalentTo(notes.First());
+        }
+        finally
+        {
+            // Clean up - delete all notes for the date
+            foreach (var note in notes)
+            {
+                await container.DeleteItemAsync<Note>(
+                    note.Id,
+                    new PartitionKey(note.Date));
+            }
+        }
+    }
+
+    private IEnumerable<Note> GetTestNotes()
+    {
+        return new List<Note>
+        {
+            new Note
+            {
+                Category = NoteCategory.General,
+                Content = "This is the first test note."
+            },
+            new Note
+            {
+                Category = NoteCategory.General,
+                Content = "This is the second test note."
+            }
         };
-
-        // Act
-        Note createdNote = await noteRepository.CreateAsync(note);
-
-        // Assert
-        createdNote.Should().NotBeNull();
-        createdNote.Id.Should().Be(note.Id);
-        createdNote.Category.Should().Be(note.Category);
-        createdNote.Content.Should().Be(note.Content);
     }
 }
