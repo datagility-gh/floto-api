ef4bd71b29183d2efcd45cdedbf6f00c9b641341
James Fox <james.fox@datagility.co.uk>
2026-04-11 11:02:00 +0100

    Adding a feature flag for notes caching (#9)

--- infra/modules/features/main.tf
+++ infra/modules/features/main.tf
@@ -6,3 +6,12 @@ resource "azurerm_app_configuration_feature" "beta" {
   label                  = var.stack
   tags                   = merge(var.tags, { delete-by = "2027/01/01" })
 }
+
+resource "azurerm_app_configuration_feature" "cached_notes" {
+  configuration_store_id = var.configuration_store_id
+  description            = "Notes read/writes are through a lazy-loaded cache."
+  name                   = "CachedNotes"
+  enabled                = true
+  label                  = var.stack
+  tags                   = merge(var.tags, { delete-by = "2026/07/31" })
+}
